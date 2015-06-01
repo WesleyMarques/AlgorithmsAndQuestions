62ba0daf73a1469c948f555e8f52955337dba24d
Wesley <wesley.torres@ccc.ufcg.edu.br>
2015-06-01 10:55:53 -0300

    Algorithm of SSCMax with PD in python(refactored)

--- PD/SSCMax.py
+++ PD/SSCMax.py
@@ -4,7 +4,7 @@ def(A,N):
 	c = [0 for i in range(N)]
 	for m in range(N):
 		c[m] = 1
-		for i in range(m-1;-1;-1):
+		for i in range(m-1,-1,-1):
 			if A[i] <= A[m] and c[i]+1 > c[m]:
 				c[m] = c[i]+1
 	return c	
