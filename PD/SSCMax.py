72d1d6dc757fa785ffd4d94e482d28eb261b8d79
Wesley <wesley.torres@ccc.ufcg.edu.br>
2015-06-01 10:50:49 -0300

    Algorithm of SSCMax with PD in python

---
+++ PD/SSCMax.py
@@ -0,0 +1,10 @@
+/*The algorithm search the max longest increasing subsequence of a list
+*/
+def(A,N):
+	c = [0 for i in range(N)]
+	for m in range(N):
+		c[m] = 1
+		for i in range(m-1;-1;-1):
+			if A[i] <= A[m] and c[i]+1 > c[m]:
+				c[m] = c[i]+1
+	return c	
